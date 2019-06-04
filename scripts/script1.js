a257dc902335c88381bf5e1049faaa878aacc976
unknown <odedwolff@gmail.com>
2019-06-04 16:11:59 +0300

    layout

--- scripts/script1.js
+++ scripts/script1.js
@@ -2725,7 +2725,7 @@ function panelInViewGrasshoppers(){
 
 function panelInViewPulsar(){
 	var topPosInViewPort = document.getElementById("viewpointDetectorPulsar").getBoundingClientRect().top;
-	return topPosInViewPort > 0 && topPosInViewPort < 1200;
+	return topPosInViewPort > 0 && topPosInViewPort < 600;
 }
 
 function panelInViewPendel(){
