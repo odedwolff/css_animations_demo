b71689ae9ecfae9409006e58a6f41bc55bcc23eb
unknown <odedwolff@gmail.com>
2019-06-13 16:51:52 +0300

    some content editing

--- scripts/strings.js
+++ scripts/strings.js
@@ -1,33 +1,35 @@
 
 
 const menuTexts={
-    "about": `this page a is a demo for some simple animation ideas of what one may call 
-    'Motion design' or something along that line, on a web context. essentially a programming exercise that  went out of control.
-    as far as concept goes, i tried to stick as much as i could to latin text characters as the sole grphic elements 
+    "about": `this page a is a demo for some simple animation ideas of what one might call 
+    'motion design' or something along that line, in web context. In essence a programming exercise gone out of control.
+    As far as concept goes, i tried to stick as much as i could to latin text characters as the sole grphic element  
 
-    Projects page at Github:
+    Projects page on Github:
     <a class='aResNote' href="https://github.com/odedwolff/css_animations_demo" target=_blank>https://github.com/odedwolff/css_animations_demo</a>
     `,
 
-    "dev_notes":`I made a point of not using any libraries, so it’s basically vanilla javascript and CSS. 
+    "dev_notes":`I made a point of not using any libraries, so it's basically vanilla javascript and CSS. 
     CSS animation is used quite heavily. In fact, the whole thing began as i tried to prove to myself 
-    That CSS animation was a terrible idea. (wasn't that bad as it turned out) 
+    that CSS animation was a terrible idea (wasn't that bad as it turned out).
     More complex animation are very hard to achieve with CSS, so i used javascript for every frame on those. 
     I did use LESS for a while, but too briefly to mention. `,
 
     "sys_notes":`Regretfully mobile platforms are not supported presently. I tried to test with 
-    A selection of Laptop/Desktop display resolutions. So hopefully it should work on these, 
-    Let me know if it looks wrong on your device (please supply device details) 
+    a selection of laptop/desktop display resolutions, so hopefully it should work on these, 
+    let me know if it looks wrong on your device (please supply device details) 
     <a href = "mailto: odedwolff@gmail.com" class='aResNote'>odedwolff@gmail.com</a>
-    Since everything is rendered in real time, some computational effort is needed, 
-    So weaker computer may have a hard time. I 
-    Tried to make it light as I could without sacrificing the animation too much
+    <br>
+    Due to the fact that everything is rendered in real time, some computational effort is needed, 
+    so weaker computers may have a hard time and yield non great looking animation.
+    What you gonna do. 
+    I tried to make it light as I could without sacrificing the animation too much
     (mainly through using lower frame rate where possible), but it 
-    Still consumes considerable resources, at least at times. 
+    still consumes considerable resources, at least at times. 
     `,
 
     "contact":`Any kind of feedback is welcome, if you want to report a bug, please supply 
-    The details of your device.
+    the details of your device.
     <a href = "mailto: odedwolff@gmail.com" class='aResNote'>odedwolff@gmail.com</a>
     
     
